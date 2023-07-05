f97e50719bc3f6e66747b32f0d407a849a61b7eb
Francisco Zapiola <franciscozapi@gmail.com>
2023-07-05 20:10:05 -0300

    Setup React Navigation

--- App.tsx
+++ App.tsx
@@ -1,18 +1,44 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- */
-
+import {NavigationContainer, useNavigation} from '@react-navigation/native';
 import {Container, Text} from '@atoms';
 import React from 'react';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import {TouchableHighlight} from 'react-native';
+
+const Stack = createNativeStackNavigator();
+
+const Home = () => {
+  const navigation = useNavigation();
+  return (
+    <Container>
+      <TouchableHighlight onPress={() => navigation.navigate('Aberturas')}>
+        <Text type="title">HouseUp</Text>
+      </TouchableHighlight>
+    </Container>
+  );
+};
+
+const Example = () => (
+  <Container>
+    <Text type="title">Example...</Text>
+  </Container>
+);
 
 function App(): JSX.Element {
   return (
-    <Container>
-      <Text type="title">HouseUp</Text>
-    </Container>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Home"
+          options={{
+            title: 'HouseUp',
+          }}
+          component={Home}
+        />
+        <Stack.Screen name="Aberturas" component={Example} />
+        <Stack.Screen name="Equipamientos" component={Example} />
+        <Stack.Screen name="Terminaciones" component={Example} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
